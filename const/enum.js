1f903e4fabfe4d34d209954facc94a4eecb492f5
Michael Guilaran <maykil093@gmail.com>
2022-10-02 20:34:04 +0800

    add user roles

--- const/enum.js
+++ const/enum.js
@@ -2,8 +2,21 @@ const accountTypes = ["customer", "merchant", "rider", "organizer"];
 const checkoutStatus = ["to-pack", "packed", "to-deliver", "delivered"];
 const riderStatus = ["active", "inactive"];
 const messageTypes = ["billing", "message", "news"];
-const userRoles = ["customer", "planner", "organizer"];
-const eventTypes = ["wedding", "kids-birthday-party", "adults-birthday-party", "disco", "casual-party"];
+const userRoles = [
+    "customer",
+    "planner",
+    "organizer",
+    "super-admin",
+    "general-organizer",
+    "general-planner"
+];
+const eventTypes = [
+    "wedding",
+    "kids-birthday-party",
+    "adults-birthday-party",
+    "disco",
+    "casual-party"
+];
 const eventStatus = ["preparing", "in-progress", "completed", "cancelled"];
 const paymentType = ["card", "cash"];
 const paymentStatus = ["unpaid", "paid"];
