45ffbd4c828e83b8501066ea51ad956c7fb2b001
Michael Guilaran <maykil093@gmail.com>
2022-10-05 03:26:49 +0800

    Add middleware for auth

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/user");
+const Profile = require("../models/profile");
+
+const protected = asyncHandler(async (req, res, next) => {
+    let token;
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            // Get token from header
+            token = req.headers.authorization.split(" ")[1];
+
+            // verify token
+            const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+            //Get user from the token
+            req.user = await User.findById(decoded.data).select("-hashValue");
+            const profile = await Profile.findOne({accountId: decoded.data});
+
+            req.user = {...req.user, role: profile.role};
+
+            next();
+        } catch (error) {
+            console.log(error);
+            res.status(400);
+            throw new Error("Not authorized!");
+        }
+    }
+
+    if (!token) {
+        res.status(400);
+        throw new Error("Not authorized! No token");
+    }
+});
+
+module.exports = {protected};
